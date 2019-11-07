750d21fa83ece99fccd1d8798f002498bc08f5a3
honda <mussolini119@163.com>
2019-11-07 16:56:21 +0800

    改到一次，增加数字记录

--- helloworld.py
+++ helloworld.py
@@ -2,9 +2,14 @@
 print("Hello,world !")
 import keyword
 import time
+
+n = 0
 for i in keyword.kwlist:
     time.sleep(3)
     print(i)
+    n += 1
+    print(n)
+
 
 lista = ("False, None, True, and, as, assert, async, await, break, class, continue, def,"
          "del, elif, else, except, finally, for, from, global, if, import, in, is, lambda,"
