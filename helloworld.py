523d80bcd336f17aee1f32510ca8e61e0ec9e61d
honda <mussolini119@163.com>
2019-11-07 17:15:15 +0800

    这次增加了一个新的要求 知识识是continue 与break

--- helloworld.py
+++ helloworld.py
@@ -4,11 +4,22 @@ import keyword
 import time
 
 n = 0
+"""
 for i in keyword.kwlist:
     time.sleep(3)
     print(i)
     n += 1
     print(n)
+"""
+for s in "python":
+    if s == "t":
+        continue
+    print(s, end="")
+print("\n","区分continue与 break")
+for s in "python":
+    if s == "t":
+        break
+    print(s, end="")
 
 
 lista = ("False, None, True, and, as, assert, async, await, break, class, continue, def,"
