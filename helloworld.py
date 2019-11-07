7d4543c33606fa00979466012e5251d527a5c50f
honda <mussolini119@163.com>
2019-11-07 16:40:59 +0800

    新建一个文件学习git pycharm 使作
    1 先进行配置 git
    2 VCS 是重点

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,11 @@
 # -*- coding:utf-8 -*-
 print("Hello,world !")
+import keyword
+import time
+for i in keyword.kwlist:
+    time.sleep(3)
+    print(i)
+
+lista = ("False, None, True, and, as, assert, async, await, break, class, continue, def,"
+         "del, elif, else, except, finally, for, from, global, if, import, in, is, lambda,"
+         "nonlocal, not, or, pass, raise, return,try, while, with, yield ")
